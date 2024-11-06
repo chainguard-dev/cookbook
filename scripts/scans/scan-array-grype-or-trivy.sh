8f431c35e4e52255d411ecea492442050900cbec
John Osborne <josborne@chainguard.dev>
2024-11-06 16:16:37 -0500

    Update scan-array-grype-or-trivy.sh
    Signed-off-by: John Osborne <josborne@chainguard.dev>

--- scripts/scans/scan-array-grype-or-trivy.sh
+++ scripts/scans/scan-array-grype-or-trivy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export GRYPE_DEFAULT_IMAGE_PULL_SOURCE=registry
-
 # Default scanner is 'grype'
 scanner="grype"
 
@@ -19,11 +17,32 @@ elif [[ -n "$1" ]]; then
 fi
 
 images=(
-  "openjdk:21-jdk"
-  "openjdk:17-jdk"
-  "openjdk:11-jdk"
-  "openjdk:8-jdk"
+  "cgr.dev/chainguard-private/dotnet-sdk:8"
+  "cgr.dev/chainguard-private/dotnet-runtime:8"
+  "cgr.dev/chainguard-private/aspnet-runtime:8"
+  "cgr.dev/chainguard-private/dotnet-sdk:6"
+  "cgr.dev/chainguard-private/dotnet-runtime:6"
+  "cgr.dev/chainguard-private/aspnet-runtime:6"
+  "cgr.dev/chainguard-private/python:3.8"
+  "cgr.dev/chainguard-private/python:3.9"
+  "cgr.dev/chainguard-private/python:3.10"
+  "cgr.dev/chainguard-private/python:3.11"
+  "cgr.dev/chainguard-private/python:3.12"
+  "cgr.dev/chainguard-private/python:3.13"
+  "cgr.dev/chainguard-private/istio-proxy:1.23"
+  "cgr.dev/chainguard-private/istio-pilot:1.23"
+  "cgr.dev/chainguard-private/istio-install-cni:1.23"
+  "cgr.dev/chainguard-private/istio-proxy:1.22"
+  "cgr.dev/chainguard-private/istio-pilot:1.22"
+  "cgr.dev/chainguard-private/istio-install-cni:1.22"
+  "cgr.dev/chainguard-private/istio-proxy:1.21"
+  "cgr.dev/chainguard-private/istio-pilot:1.21"
+  "cgr.dev/chainguard-private/istio-install-cni:1.21"
+  "cgr.dev/chainguard-private/node:22"
+  "cgr.dev/chainguard-private/node:20"
+  "cgr.dev/chainguard-private/node:18"
 )
+
 
 echo ""
 echo "Image Size On Disk:"
@@ -59,6 +78,11 @@ totalMedium=0
 totalLow=0
 totalWontFix=0
 totalCount=0
+totalfixedCritical=0
+totalfixedHigh=0
+totalfixedMedium=0
+totalfixedLow=0
+totalfixedCount=0
 
 echo "Scanning images..."
 for image in "${!image_digests[@]}"; do
@@ -67,16 +91,72 @@ done
 
 for IMAGE in "${images[@]}"; do
   
-  if [[ "$scanner" == "grype" ]]; then
-    # Grype
-    output=$(grype $IMAGE -o json 2>/dev/null | jq -c '{Total: [.matches[].vulnerability] | length, Critical: [.matches[] | select(.vulnerability.severity == "Critical")] | length, High: [.matches[] | select(.vulnerability.severity == "High")] | length, Medium: [.matches[] | select(.vulnerability.severity == "Medium")] | length, Low: [.matches[] | select(.vulnerability.severity == "Low")] | length, WontFix: [.matches[] | select(.vulnerability.fix.state == "wont-fix")] | length }')
-    critical=$(jq '.Critical' <<< "$output")
-    high=$(jq '.High' <<< "$output")
-    medium=$(jq '.Medium' <<< "$output")
-    low=$(jq '.Low' <<< "$output")
-    wontfix=$(jq '.WontFix' <<< "$output")
-    total=$(jq '.Total' <<< "$output")
+ if [[ "$scanner" == "grype" ]]; then
+ 
+  # Grype
+  output=$(grype "$IMAGE" -o json 2>/dev/null | jq -c '{
+    Total: [.matches[].vulnerability] | length,
+    Critical: [.matches[] | select(.vulnerability.severity == "Critical")] | length,
+    High: [.matches[] | select(.vulnerability.severity == "High")] | length,
+    Medium: [.matches[] | select(.vulnerability.severity == "Medium")] | length,
+    Low: [.matches[] | select(.vulnerability.severity == "Low")] | length,
+    WontFix: [.matches[] | select(.vulnerability.fix.state == "wont-fix")] | length,
+    FixedTotal: [.matches[] | select((.vulnerability.fix.state | ascii_downcase) == "fixed")] | length,
+    FixedCritical: [.matches[] | select((.vulnerability.severity | ascii_downcase) == "critical" and (.vulnerability.fix.state | ascii_downcase) == "fixed")] | length,
+    FixedHigh: [.matches[] | select((.vulnerability.severity | ascii_downcase) == "high" and (.vulnerability.fix.state | ascii_downcase) == "fixed")] | length,
+    FixedMedium: [.matches[] | select((.vulnerability.severity | ascii_downcase) == "medium" and (.vulnerability.fix.state | ascii_downcase) == "fixed")] | length,
+    FixedLow: [.matches[] | select((.vulnerability.severity | ascii_downcase) == "low" and (.vulnerability.fix.state | ascii_downcase) == "fixed")] | length
+  }')
   
+  critical=$(jq '.Critical' <<< "$output")
+  high=$(jq '.High' <<< "$output")
+  medium=$(jq '.Medium' <<< "$output")
+  low=$(jq '.Low' <<< "$output")
+  wontfix=$(jq '.WontFix' <<< "$output")
+  total=$(jq '.Total' <<< "$output")
+  fixed_critical=$(jq '.FixedCritical' <<< "$output")
+  fixed_high=$(jq '.FixedHigh' <<< "$output")
+  fixed_medium=$(jq '.FixedMedium' <<< "$output")
+  fixed_low=$(jq '.FixedLow' <<< "$output")
+  fixed_total=$(jq '.FixedTotal' <<< "$output")
+
+  json=$(jq --arg image "$IMAGE" \
+    --arg critical "$critical" \
+    --arg high "$high" \
+    --arg medium "$medium" \
+    --arg low "$low" \
+    --arg wontfix "$wontfix" \
+    --arg total "$total" \
+    --arg fixed_critical "$fixed_critical" \
+    --arg fixed_high "$fixed_high" \
+    --arg fixed_medium "$fixed_medium" \
+    --arg fixed_low "$fixed_low" \
+    --arg fixed_total "$fixed_total" \
+    '.items += [{
+      image: $image,
+      scan: {
+        type: "grype",
+        critical: ($critical | tonumber),
+        high: ($high | tonumber),
+        medium: ($medium | tonumber),
+        low: ($low | tonumber),
+        wontfix: ($wontfix | tonumber),
+        total: ($total | tonumber),
+        fixed_critical: ($fixed_critical | tonumber),
+        fixed_high: ($fixed_high | tonumber),
+        fixed_medium: ($fixed_medium | tonumber),
+        fixed_low: ($fixed_low | tonumber),
+        fixed_total: ($fixed_total | tonumber)
+      }
+    }]' <<< "$json")
+
+    totalfixedCritical=$((totalfixedCritical + fixed_critical))
+    totalfixedHigh=$((totalfixedHigh + fixed_high))
+    totalfixedMedium=$((totalfixedMedium + fixed_medium))
+    totalfixedLow=$((totalfixedLow + fixed_low))
+    totalfixedCount=$((totalfixedCount + fixed_total))
+
+
   elif [[ "$scanner" == "trivy" ]]; then
     # Trivy
     output=$(trivy image -f json "$IMAGE" 2>/dev/null | jq -c 'if (.Results | length) == 0 then { Total: 0, Critical: 0, High: 0, Medium: 0, Low: 0, WontFix: 0 } else [.Results[] | select(has("Vulnerabilities")) | .Vulnerabilities[]] | { Total: length, Critical: (map(select(.Severity == "CRITICAL")) | length), High: (map(select(.Severity == "HIGH")) | length), Medium: (map(select(.Severity == "MEDIUM")) | length), Low: (map(select(.Severity == "LOW")) | length), WontFix: (map(select(.Status == "will_not_fix")) | length)} end')
@@ -86,10 +166,8 @@ for IMAGE in "${images[@]}"; do
     low=$(jq '.Low' <<< "$output")
     wontfix=$(jq '.WontFix' <<< "$output")
     total=$(jq '.Total' <<< "$output")
-  fi
-  
-  echo "$output"
-  json=$(jq --arg image "$IMAGE" \
+
+    json=$(jq --arg image "$IMAGE" \
           --arg critical "$critical" \
           --arg high "$high" \
           --arg medium "$medium" \
@@ -109,6 +187,7 @@ for IMAGE in "${images[@]}"; do
             }
           }]' <<< "$json")
 
+  fi
 
   totalCritical=$((totalCritical + critical))
   totalHigh=$((totalHigh + high))
@@ -139,7 +218,29 @@ echo -n "Average High CVEs: "; echo "scale=2; $totalHigh / ${#images[@]}" | bc
 echo -n "Average Medium CVEs: "; echo "scale=2; $totalMedium / ${#images[@]}" | bc
 echo -n "Average Low CVEs: "; echo "scale=2; $totalLow / ${#images[@]}" | bc
 
+if [[ "$scanner" == "grype" ]]; then
+  
+  averagefixedCritical=$((totalfixedCritical / ${#images[@]}))
+  averagefixedHigh=$((totalfixedHigh / ${#images[@]}))
+  averagefixedMedium=$((totalfixedMedium / ${#images[@]}))
+  averagefixedLow=$((totalfixedLow / ${#images[@]}))
+
+  echo ""
+  echo "Total Fixes Available: $totalfixedCount"
+  echo "Total Critcal Fixes Available: $totalfixedCritical"
+  echo "Total High Fixes Available: $totalfixedHigh"
+  echo "Total Medium Fixes Available: $totalfixedMedium"
+  echo "Total Low Fixes Available: $totalfixedLow"
+  echo -n "Average Fixes Available: "; echo "scale=2; $totalCount / ${#images[@]}" | bc
+  echo -n "Average Critcal Fixes Available: "; echo "scale=2; $totalCritical / ${#images[@]}" | bc
+  echo -n "Average High Fixes Available: "; echo "scale=2; $totalHigh / ${#images[@]}" | bc
+  echo -n "Average Medium Fixes Available: "; echo "scale=2; $totalMedium / ${#images[@]}" | bc
+  echo -n "Average Low Fixes Available: "; echo "scale=2; $totalLow / ${#images[@]}" | bc
+  echo ""
+
+fi
+
 echo "JSON Output:"
 echo "$json"
 echo "CSV Output:"
-echo "$json" | jq -r '.items[] | [.image, .scan.total, .scan.critical, .scan.high, .scan.medium, .scan.low, .scan.wontfix] | @csv'
+echo "$json" | jq -r '.items[] | [.image, .scan.total, .scan.critical, .scan.high, .scan.medium, .scan.low, .scan.wontfix, .scan.fixed_total, .scan.fixed_critical, .scan.fixed_high, .scan.fixed_medium, .scan.fixed_low] | @csv'
