e8da630df5ce847a1eda7001162250a4eb0facc7
Madhav Mehta <madhav.mehta@chainguard.dev>
2025-10-29 21:04:17 +0000

    Initial Go Lambda example commit (#24)
    add readme
    
    ---------
    
    Signed-off-by: Madhav Mehta <madhav.mehta@chainguard.dev>

---
+++ aws-lambda-go/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	response := events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "\"Hello from Lambda!\"",
+	}
+	return response, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
